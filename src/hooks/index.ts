93c6450ce412688a5ecbd5b9d349317fefcbb05e
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-09-23 15:01:41 -0600

    Export StaticBreakpoint from hooks index
    Adds StaticBreakpoint export to src/hooks/index.ts for easier access to static breakpoint utility from the hooks module.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,3 +6,6 @@ export * from './useBreakpointCondition';
 export * from './useBreakpointContainer';
 export * from './useBreakpointContainerCondition';
 export * from './useContainerBreakpoint';
+
+// Static breakpoint as utility
+export { StaticBreakpoint } from '../const/breakpoints';
