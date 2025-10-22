4cf61589a57a7334e04c4c50a6f6b5119fdab50a
Carlos Guerra <CarlosEGuerraSilva@hotmail.com>
2025-10-22 13:55:01 -0600

    feat: export StaticBreakpointContainer and component props
    Exports StaticBreakpointContainer from breakpoints and ContainerProps/GridProps types from their respective components.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,4 +4,4 @@ export * from './use-breakpoint-container';
 export * from './use-breakpoint-container-condition';
 export * from './use-container-breakpoint';
 export * from './use-breakpoint-helpers';
-export { StaticBreakpoint } from '../const/breakpoints';
+export { StaticBreakpoint, StaticBreakpointContainer } from '../const/breakpoints';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './hooks';
 export { default as Container } from './components/Container';
 export { default as Grid } from './components/Grid';
+export type { ContainerProps } from './components/Container';
+export type { GridProps } from './components/Grid';
